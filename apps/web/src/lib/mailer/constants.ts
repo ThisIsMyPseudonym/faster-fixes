24bf93cd0525c94eb43740a6641ce219862c6d88
Matt-MFG <matthew.t.marshall@hotmail.com>
2026-07-13 12:00:10 -0500

    fix: allow verified email sender domain

--- apps/web/src/lib/mailer/constants.ts
+++ apps/web/src/lib/mailer/constants.ts
@@ -1,2 +1,6 @@
-export const NO_REPLY_EMAIL = `noreply@${process.env.DOMAIN_NAME}`;
-export const SENDER_EMAIL = `contact@${process.env.DOMAIN_NAME}`;
+const mailDomain = process.env.DOMAIN_NAME;
+
+export const NO_REPLY_EMAIL =
+  process.env.MAIL_FROM_EMAIL ?? `noreply@${mailDomain}`;
+export const SENDER_EMAIL =
+  process.env.MAIL_FROM_EMAIL ?? `contact@${mailDomain}`;
